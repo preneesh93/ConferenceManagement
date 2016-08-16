92c570ce4425e4362065e5a0a6115eb4223d0ae1
Lokesh Kumar J R <lokesh.kumar20012012@live.com>
2016-08-16 12:21:33 +0200

    Able to push submission id to user object and submission data to submission object.

--- api/routes/submissions.js
+++ api/routes/submissions.js
@@ -2,26 +2,32 @@
  * Created by Preneesh on 12-06-2016.
  */
 var Sub =  require('../schemas/submissions');
+var User   = require('../schemas/users');
 
-/*module.exports.getSub = function (req,res) {
-  console.log("getting project details");
-  var conditions = {title: req.query.title};
-  Sub.findOne(conditions,function (err,result) {
-    if(err){throw err}
-    res.json(result);
-    console.log("Submission was successful");
-  });
+module.exports.list = function (req, res){
+  Sub.find(function (err,result) {res.send(result)})
 };
-*/
 
 module.exports.postSub = function (req, res) {
   console.log("Getting the Submission");
   var add = new Sub(req.body);
-  add.save(function (err,result) {
+  add.save(function (err,result1) {
     if(err) { throw err; }
     else {
-      res.send(result);
+      console.log(result1);
+      console.log("submission was successful");
+      var conditions = {username: req.query.username};
+      var update = {$push: {submission: result1}};
+      User.findOneAndUpdate(conditions, update, {new: true}, function (err1, result) {
+        if (err1) {
+          throw err1;
+        }
+        else {
+          res.json(result);
+          console.log(result);
+        }
+      });
     }
-    console.log("submission was successful");
-    });
+  });
 };
+  
