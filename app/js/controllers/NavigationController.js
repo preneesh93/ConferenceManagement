ee795df2708890dce19d9e776435bf45570f8dd8
giri <g.sivadanam@patagona.de>
2016-09-07 23:35:53 +0200

    added mobile device support and hides on small screens

--- app/js/controllers/NavigationController.js
+++ app/js/controllers/NavigationController.js
@@ -2,8 +2,20 @@
  * Created by Girish on 5/24/2016.
  */
 angular.module('cms')
-  .controller('NavigationController', function($scope, $rootScope, $window,$state){
+  .controller('NavigationController', function($scope, $rootScope, $window,$state,$mdSidenav,$mdMedia){
+    $scope.hide = function () {
+      return $mdMedia('gt-sm')
+    }
+    $scope.toggleLeft = function () {
+      console.log("toggling")
+        // Component lookup should always be available since we are not using `ng-if`
+      $mdSidenav('left')
+        .toggle()
+        .then(function () {
+          console.log("toggle is done");
+        });
 
+    }
     $scope.logout=function () {
       $rootScope.isAuthenticated=false;
       $window.localStorage.clear();
